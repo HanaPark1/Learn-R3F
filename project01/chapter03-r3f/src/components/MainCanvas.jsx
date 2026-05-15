59479a9eb43d66a366df3a27427fbd0fe329ef88
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2026-05-15 21:17:40 +0900

    study: r3f 기본구성(Geometry, Material)

--- project01/chapter03-r3f/src/components/MainCanvas.jsx
+++ project01/chapter03-r3f/src/components/MainCanvas.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei"
 import { Color } from "three";
+import { Meshes } from "./Meshes";
 
 export const MainCanvas = () => {
     return (
         // gl = 끝마무리 처리 완화 (렌더러 대응)
         <Canvas 
         gl={{antialias: true}}
+        shadows={"soft"} // 그림자 사용
         camera={{
             fov:60,
             aspect: window.innerWidth / window.innerHeight, // 카메라 비율
@@ -15,10 +18,21 @@ export const MainCanvas = () => {
         }}
         scene={{background: new Color(0x000000)}}
         >
-            <mesh position={[0,0,0]}>
-                <boxGeometry args={[1,1,1]} />
-                <meshBasicMaterial color={0xff0000}/>
-            </mesh>
+            <OrbitControls />
+            <directionalLight 
+                castShadow
+                args={[0xffffff, 5]}
+                position={[4,4,4]}
+                shadow-camera-left={-25}
+                shadow-camera-right={25}
+                shadow-camera-top={25}
+                shadow-camera-bottom={-25}
+                shadow-camera-near={0.1}
+                shadow-camera-far={1000}
+                shadow-mapSize-width={4096}
+                shadow-mapSize-height={4096}
+            />
+            <Meshes/>
         </Canvas>
     );
 }
