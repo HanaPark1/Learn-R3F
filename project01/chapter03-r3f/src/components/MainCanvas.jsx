5adcc65bdcf92c5e28303ef4745cbd904d2a2ce0
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2026-05-15 21:42:54 +0900

    study: r3f 기본구성(light)

--- project01/chapter03-r3f/src/components/MainCanvas.jsx
+++ project01/chapter03-r3f/src/components/MainCanvas.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei"
 import { Color } from "three";
 import { Meshes } from "./Meshes";
+import { Lights } from "./Lights";
 
 export const MainCanvas = () => {
     return (
@@ -19,19 +20,8 @@ export const MainCanvas = () => {
         scene={{background: new Color(0x000000)}}
         >
             <OrbitControls />
-            <directionalLight 
-                castShadow
-                args={[0xffffff, 5]}
-                position={[4,4,4]}
-                shadow-camera-left={-25}
-                shadow-camera-right={25}
-                shadow-camera-top={25}
-                shadow-camera-bottom={-25}
-                shadow-camera-near={0.1}
-                shadow-camera-far={1000}
-                shadow-mapSize-width={4096}
-                shadow-mapSize-height={4096}
-            />
+
+            <Lights/>
             <Meshes/>
         </Canvas>
     );
